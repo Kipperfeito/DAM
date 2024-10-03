3dcb5497a5e7229e34c61006745d5c1b42975c7a
Kipperfeito <heitor.k.w@gmail.com>
2024-10-03 08:41:12 -0300

    Pronto

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Pressable, Image,} from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  const [pedido, setPedido] = useState([]);
+  const [pedido, setPedido] = useState([]); //Declaro array
 
   const selecionarItem = (item) => {
-    setPedido([...pedido, item]);  // Adiciona o item ao pedido
+    setPedido([...pedido, item]);  // Adiciona o item ao array
     alert(`${item.nome} selecionado`);
   };
 
   const encerrarPedido = () => {
     if (pedido.length === 0) {
       alert('Nenhum item selecionado');
-    }  else if(pedido.length === 1) {
+    }  
+    //Função que soma os preco dos pedidos e usa o length para saber quantos tem
+    else if(pedido.length === 1) {
       const total = pedido.reduce((acc, item) => acc + item.preco, 0); 
-      alert(`Você selecionou ${pedido.length} item. Valor total: R$ ${total.toFixed(2)}`);
+      alert(`Você selecionou ${pedido.length} item. Valor total: R$ ${total.toFixed(2)}`); //Caso tenha apenas 1 pedido, fica no singular.
     }  else {
-      const total = pedido.reduce((acc, item) => acc + item.preco, 0); 
+      const total = pedido.reduce((acc, item) => acc + item.preco, 0); // Se não, fica no plural
       alert(`Você selecionou ${pedido.length} itens. Valor total: R$ ${total.toFixed(2)}`);
     }
   };
@@ -35,17 +36,17 @@ export default function App() {
           </Pressable>
         </View>
         <View style={styles.view2}>
-          <Pressable style={styles.view2} onPress={() => selecionarItem({ nome: 'Frango', preco: 5.00 })}>
+          <Pressable style={styles.view2} onPress={() => selecionarItem({ nome: 'Frango', preco: 4.00 })}>
             <Image source={require('./assets/Frango.jpg')} style={styles.imagem} />
             <Text style={styles.titulos}>Frango</Text>
-            <Text style={styles.preco}>R$ 5,00</Text>
+            <Text style={styles.preco}>R$ 4,00</Text>
           </Pressable>
         </View>
         <View style={styles.view3}>
-          <Pressable style={styles.view3} onPress={() => selecionarItem({ nome: 'Queijo', preco: 5.00 })}>
+          <Pressable style={styles.view3} onPress={() => selecionarItem({ nome: 'Queijo', preco: 3.00 })}>
             <Image source={require('./assets/Queijo.jpeg')} style={styles.imagem} />
             <Text style={styles.titulos}>Queijo</Text>
-            <Text style={styles.preco}>R$ 5,00</Text>
+            <Text style={styles.preco}>R$ 3,00</Text>
           </Pressable>
         </View>
         <View style={styles.view4}>
